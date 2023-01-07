96611fbc4ac7b71d5a14c141a22e6b1bd1117915
nikitarubcov99 <nikitarubstov99@gmail.com>
2023-01-07 04:19:57 +0300

    fix

--- scripts/wait-script.sh
+++ scripts/wait-script.sh
@@ -2,7 +2,9 @@
 
 IFS="," read -ra PORTS <<<"$WAIT_PORTS"
 path=$(dirname "$0")
-
+echo "Sleeping for 90 seconds"
+sleep 90
+echo "Wake up"
 PIDs=()
 for port in "${PORTS[@]}"; do
   "$path"/wait-for.sh -t 120 "http://localhost:$port/manage/health" -- echo "Host localhost:$port is active" &
