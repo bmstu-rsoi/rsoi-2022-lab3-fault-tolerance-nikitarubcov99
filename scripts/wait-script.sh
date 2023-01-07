28cbc16c5e3970401eb438bccea79ff65858ca03
nikitarubcov99 <nikitarubstov99@gmail.com>
2023-01-07 04:47:05 +0300

    fix

--- scripts/wait-script.sh
+++ scripts/wait-script.sh
@@ -6,11 +6,15 @@ echo "Sleeping for 30 seconds"
 sleep 90
 echo "Wake up"
 
+
 PIDs=()
 for port in "${PORTS[@]}"; do
+  echo "Starts health checking..."
   "$(pwd)"/scripts/wait-for.sh -t 120 "http://localhost:$port/manage/health" -- echo "Host localhost:$port is active" &
   PIDs+=($!)
+  echo "Health checking complete!"
 done
+
 
 for pid in "${PIDs[@]}"; do
   if ! wait "${pid}"; then
